2460bdb5036fcc742d3ca68c5be8617cb95117c5
Gabriel Surculento <gespejo121@gmail.com>
2022-03-04 17:06:53 -0300

    Mock y Normalizacion - Modificando mensajes

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ const express = require('express')
 const http = require('http')
 const path = require('path')
 const router = require('./routers/app.routers')
-// import { router } from "./routers/app.routers.js";
 const { ProductosDaoMongoDb } = require('./models/index')
-// import { ProductosApi } from "./models/productos/productos.api.js";
 const { ChatDaoMongoDb } = require('./models/index')
-// import { ChatApi } from "./models/daos/chat/ChatDaoMongoDb.js";
 
 const app = express()
 const server = http.createServer(app)
@@ -17,8 +14,7 @@ const productos = new ProductosDaoMongoDb("productos")
 const PORT = process.env.PORT || 8080;
 
 
-// Template Engines
-app.use('/static', express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
 // Rutas
 app.get('/', (req, res) => {
@@ -30,11 +26,11 @@ io.on('connection', socket => {
   emitir()
 
   socket.on("incomingMessage", message =>{
-    chat.push(message)
+    chat.guardar(message)
     emitir()
   })
 })
 
-const emitir = () => io.sockets.emit("chat", chat)
+const emitir = async () => await io.sockets.emit("chat", chat.listar())
 
 server.listen(PORT, () => { console.log(`Running on port: ${PORT}`)})
--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,9 @@ const button = document.getElementById("buttonSocket")
 const sendMessage = (e) => {
   const hoy = new Date()
   const message = {
-    mail: document.getElementById("nombreUsuario").value,
+    author: {
+      alias: document.getElementById("nombreUsuario").value
+    },
     text: document.getElementById("mensaje").value,
     fecha: hoy.toLocaleDateString() + ' ' + hoy.getHours() + ':' + hoy.getMinutes() + ':' + hoy.getSeconds()
   };
@@ -21,7 +23,7 @@ socket.on("chat", messages => {
   const texto = messages.map( mensaje => {
     return(`
     <div>
-      <strong class="azul">${mensaje.mail} </strong>
+      <strong class="azul">${mensaje.author.alias} </strong>
       [<span class="marron">${mensaje.fecha}</span>]:
       <em class="verde"> ${mensaje.text}</em>
     </div>`);
